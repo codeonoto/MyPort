4739577d8d62b265fc066301d57607f3859e522e
Deepanshu Sahu <116287792+codeonoto@users.noreply.github.com>
2024-08-01 11:43:24 +0530

    fixing bug

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,16 +6,8 @@ import { socialMedia } from '@/data';
 function Footer() {
   return (
     <footer
-      className='w-full pt-20 pb-10'
+      className='w-full pb-10 mb-[100px] md:mb-5'
       id='contact'>
-      <div className='w-full absolute left-0 -bottom-72 min-h-96'>
-        <img
-          src='/footer-grid.svg'
-          alt='grid'
-          className='w-full h-full opacity-50'
-        />
-      </div>
-
       <div className='flex flex-col items-center'>
         <h1 className='heading lg:max-w-[45vw]'>
           Ready To Take <span className='text-purple'>Your</span> Digital
--- components/ui/FloatingNav.tsx
+++ components/ui/FloatingNav.tsx
@@ -67,7 +67,7 @@ export const FloatingNav = ({
               'relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500'
             )}>
             <span className='block sm:hidden'>{navItem.icon}</span>
-            <span className='hidden sm:block text-sm'>{navItem.name}</span>
+            <span className='text-sm !cursor-pointer'>{navItem.name}</span>
           </Link>
         ))}
       </motion.div>
