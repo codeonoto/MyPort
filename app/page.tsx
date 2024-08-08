5fc8a3bcb54a28bf3132c7e9ffbfd3d2ea474a2b
Deepanshu Sahu <116287792+codeonoto@users.noreply.github.com>
2024-08-08 15:04:46 +0530

    hmmm

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
-        <RecentProject />
+        {/* <RecentProject /> */}
         <Clients />
         <Experience />
         <Approach />
--- data/index.ts
+++ data/index.ts
@@ -2,7 +2,7 @@ import { url } from 'inspector';
 
 export const navItems = [
   { name: 'About', link: '#about' },
-  { name: 'Projects', link: '#projects' },
+  // { name: 'Projects', link: '#projects' },
   { name: 'Testimonials', link: '#testimonials' },
   { name: 'Contact', link: '#contact' },
 ];
